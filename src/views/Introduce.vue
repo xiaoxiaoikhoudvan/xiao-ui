9594a9d5b610c2250501dd13bd713285822ad1cf
xiaoxiaoikhoudvan <752998553@qq.com>
2022-10-30 16:54:37 +0800

    介绍页面添加预览地址

--- src/views/Introduce.vue
+++ src/views/Introduce.vue
@@ -4,6 +4,7 @@
         <p>Xiao UI 是一款基于 Vue3 的 UI 组件库。</p>
         <p>此 UI 库仅作为个人造 UI 组件轮子的一次尝试。</p>
         <p>所以此 UI 库不建议用于生产环境，但可用来学习如何手写 UI 组件库，每个组件都有对应的代码示例和使用方法。</p>
+        <p>源码已经上传到我的个人github：<a href="https://github.com/xiaoxiaoikhoudvan/xiao-ui.git">源码地址</a></p>
         <!-- 下一节：<a href="#/doc/install">安装</a> -->
     </article>
 </template>
